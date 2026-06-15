6bc9602e60ab80cfa5cf21186d0024e4b170d37e
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2026-06-15 23:12:20 +0530

    Add isomorphic string program

--- String/IsomorphicString.java
+++ String/IsomorphicString.java
@@ -19,12 +19,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        int [][] mat = {
-            {1,2,3},
-            {2,3,4},
-            {5,6,7}
-        };
-
-        int  result =  di
+       
     }
 }
