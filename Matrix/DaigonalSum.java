3f7a5c4b7629f3243e360a2e390792c4808b6dd0
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2026-06-15 12:04:22 +0530

    1572. Matrix Diagonal Sum

---
+++ Matrix/DaigonalSum.java
@@ -0,0 +1,30 @@
+public  class DaigonalSum {
+
+    public static int daigonalSum( int [][] mat){
+
+int m = mat.length;
+
+int n = mat[0].length;
+
+int sum=0;
+
+ for(int i = 0; i <m;i++){
+
+    sum += mat[i][i];
+    
+    sum = sum+ mat[i][n-i-1] ;
+
+    if(n % 2 == 0){
+        
+    }
+}
+
+return sum;
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+    Dai
