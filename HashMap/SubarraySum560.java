ae9d685d94cf4ab5305ac4c1b2c42dd0bccc0e65
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2026-07-04 15:18:56 +0530

    560. Subarray Sum Equals K

---
+++ HashMap/SubarraySum560.java
@@ -0,0 +1,27 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class SubarraySum560 {
+
+    public static int subarraySum(int nums[], int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0, count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            sum += nums[i];
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+}
